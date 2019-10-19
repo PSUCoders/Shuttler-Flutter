ea1e9f335f0c70a8f20d8cb478ecd4f8a7f5ffcd
Quan Vo <votrungquan99@gmail.com>
2019-10-18 23:07:55 -0400

    add no notification animation

---
+++ lib/screens/notification/no_notification.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        VideoPlayerController.asset('assets/animations/no-notifications.mp4')
+          ..initialize().then((_) {
+            _controller.play();
+            _controller.setLooping(true);
+
+            // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+            setState(() {});
+          });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        backgroundColor: new Color.fromRGBO(255, 254, 255, 1),
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                )
+              : Container(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
