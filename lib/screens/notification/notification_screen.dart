cb5c5068a3e3a688d5fa9dd39611b6377d312313
Hung Nguyen <nthungdev@gmail.com>
2019-11-01 18:58:48 -0400

    Add Notification Screen - Add Notification Tile

--- lib/screens/notification/notification_screen.dart
+++ lib/screens/notification/notification_screen.dart
@@ -1,18 +1,75 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart' hide Notification;
+import 'package:flutter/material.dart' hide Notification;
+
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:shuttler_flutter/providers/notification_state.dart';
-import 'package:shuttler_flutter/widgets/notification_tile.dart';
+import 'package:rect_getter/rect_getter.dart';
+import 'package:shuttler/providers/notification_state.dart';
+import 'package:shuttler/widgets/notification_tile.dart';
+import 'package:shuttler/models/notification.dart';
 
 /// Notification Screen
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  var _itemKeys = {};
+  var _listViewKey = RectGetter.createGlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    // Mark read notifications after first frame
+    WidgetsBinding.instance.addPostFrameCallback((_) => _handleScroll(null));
+  }
+
+  bool _handleScroll(ScrollUpdateNotification scrollUpdateNotification) {
+    print("handling scroll update...");
+    final indexes = _getVisible();
+    final notifications = Provider.of<NotificationState>(context).notifications;
+    List<Notification> notis = [];
+    indexes.forEach((index) {
+      notis.add(notifications[index]);
+    });
+    _handleVisibleItemChange(notis);
+    return true;
+  }
+
+  List<int> _getVisible() {
+    /// First, get the rect of ListView, and then traver the _keys
+    /// get rect of each item by keys in _keys, and if this rect in the range of ListView's rect,
+    /// add the index into result list.
+    var rect = RectGetter.getRectFromKey(_listViewKey);
+    var _items = <int>[];
+    _itemKeys.forEach((index, key) {
+      var itemRect = RectGetter.getRectFromKey(key);
+      if (itemRect != null &&
+          !(itemRect.top > rect.bottom || itemRect.bottom < rect.top))
+        _items.add(index);
+    });
+
+    /// so all visible item's index are in this _items.
+    return _items;
+  }
+
+  _handleVisibleItemChange(List<Notification> items) {
+    // TODOS
+    final notificationState = Provider.of<NotificationState>(context);
+    items.forEach((item) {
+      if (item.seen) return;
+      notificationState.markAsSeen(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final notifications =
-        Provider.of<NotificationState>(context).notifications ?? [];
+    print('building notification screen...');
+    final notificationState = Provider.of<NotificationState>(context);
+    final notifications = notificationState.notifications ?? [];
 
-    // final notifications = []; // Uncomment to mimic empty notification
+    print('Notification Screen: notifications $notifications');
 
     return PlatformScaffold(
       appBar: PlatformAppBar(
@@ -25,14 +82,24 @@ class NotificationScreen extends StatelessWidget {
                   // TODO replace with appropriate child
                   child: Text("Empty"),
                 )
-              : ListView.separated(
-                  itemCount: notifications.length,
-                  separatorBuilder: (_, index) => Divider(),
-                  itemBuilder: (context, index) {
-                    return NotificationTile(
-                      notification: notifications[index],
-                    );
-                  },
+              : RectGetter(
+                  key: _listViewKey,
+                  child: NotificationListener<ScrollUpdateNotification>(
+                    onNotification: _handleScroll,
+                    child: ListView.separated(
+                      itemCount: notifications.length,
+                      separatorBuilder: (_, index) => Divider(height: 0),
+                      itemBuilder: (context, index) {
+                        _itemKeys[index] = RectGetter.createGlobalKey();
+                        return RectGetter(
+                          key: _itemKeys[index],
+                          child: NotificationTile(
+                            notification: notifications[index],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
         ),
       ),
--- lib/widgets/notification_tile.dart
+++ lib/widgets/notification_tile.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-import 'package:shuttler_flutter/models/notification.dart' as N;
+import 'package:flutter/cupertino.dart' hide Notification;
+import 'package:flutter/material.dart' hide Notification;
+import 'package:intl/intl.dart';
+import 'package:shuttler/models/notification.dart';
+import 'package:shuttler/utilities/theme.dart';
 
 class NotificationTile extends StatelessWidget {
   const NotificationTile({
@@ -12,20 +13,64 @@ class NotificationTile extends StatelessWidget {
         super(key: key);
 
   final VoidCallback onPressed;
-  final N.Notification notification;
+  final Notification notification;
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: ListTile(
-        title: Text(
-          notification.title,
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-          ),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Title of the notification
+                Expanded(
+                  child: Text(
+                    notification.title,
+                    style: ShuttlerTheme.of(context).textTheme.headline,
+                  ),
+                ),
+                // Description of the notification
+              ],
+            ),
+            SizedBox(height: 5),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    notification.description,
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Text(
+                      DateFormat("EEEE, MMM dd, yyyy '•' h:mm a")
+                          .format(this.notification.date),
+                      style: ShuttlerTheme.of(context).textTheme.display1,
+                    ),
+                  ),
+                ),
+                Icon(
+                  this.notification.seen
+                      ? Icons.check_circle
+                      : Icons.check_circle_outline,
+                  size: 14,
+                  color: Colors.black54,
+                )
+              ],
+            ),
+          ],
         ),
-        subtitle: Text(notification.description),
       ),
     );
   }
