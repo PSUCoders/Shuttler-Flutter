42f96a7a14a2edd10d3b25da7b90f5019e4650e1
Hung Nguyen <nthungdev@gmail.com>
2019-11-19 20:40:25 -0500

    Remove clutter code

--- lib/screens/map/map_layout.dart
+++ lib/screens/map/map_layout.dart
@@ -1,8 +1,5 @@
 import 'dart:async';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
 
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
